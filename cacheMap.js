a293918fa1f9f170c2ba26ce442fe540029fc1d2
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-11-13 15:17:20 +0600

    Code added

---
+++ cacheMap.js
@@ -0,0 +1,23 @@
+const dataCache = new Map();
+
+const expensiveTask = (id) => {
+    console.log("Ran the expensive task", id);
+    return { id: id, data: `Some data for id: ${id}`, timestamp: new Date().getTime() }
+}
+
+const getData = (id) => {
+    if (dataCache.has(id)) {
+        console.log("Cache hit for", id);
+        return dataCache;
+    }
+    console.log("Cache missed for", id);
+    const data = expensiveTask(id)
+    console.log(dataCache);
+    dataCache.set(id, data)
+    return dataCache
+}
+
+console.log(getData(123));
+console.log(getData(123));
+// console.log(getData(111));
+// console.log(getData(333));
